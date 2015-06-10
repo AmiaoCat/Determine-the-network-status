da2cb19d12b314f60def85cf09b2ea90ca8b7dd7
Ss <ss@Ss.local>
2015-06-10 16:08:25 +0800

    Initial Commit

---
+++ 判断当前网络状态/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  判断当前网络状态
+//
+//  Created by Ss on 15/6/10.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 判断当前网络状态/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  判断当前网络状态
+//
+//  Created by Ss on 15/6/10.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
